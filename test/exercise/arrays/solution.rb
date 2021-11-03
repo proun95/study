236367e4b8aca49a101d8985dadffb06bb8d1b2f
Aleksandr Sirotin <proun95@gmail.com>
2021-11-03 15:54:17 +0400

    add replace method

--- test/exercise/arrays/solution.rb
+++ test/exercise/arrays/solution.rb
@@ -1,8 +1,28 @@
 module Exercise
   module Arrays
     class << self
+      def get_max(array, max = array[0])
+        head, *tail = array
+        if array == []
+          return max
+        elsif head > max
+          max = head
+        end
+
+        get_max(tail, max)
+      end
+
+      def replace_with_value(array, value)
+        head, *tail = array
+        head = value if head.positive?
+        return head if tail == []
+
+        [head, *replace_with_value(tail, value)]
+      end
+
       def replace(array)
-        array
+        max = get_max(array)
+        replace_with_value(array, max)
       end
 
       def search(_array, _query)
