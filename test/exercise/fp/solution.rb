0787f786e8f78b19f9628069381da41aba94ea54
Aleksandr Sirotin <proun95@gmail.com>
2021-11-03 19:17:22 +0400

    refactor rating method

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -5,18 +5,21 @@ module Exercise
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
       def is_correct_film(film)
-        !film['country'].nil? && film['country'].include?(',') && !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f > 0 ? true : false
+        has_more_than_two_contries = !film['country'].nil? && film['country'].include?(',')
+        has_rating = !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f > 0
+
+        has_more_than_two_contries && has_rating
       end
 
       def rating(array)
-        film_raiting = array.each_with_object({ raiting_sum: 0.0, count: 0 }) do |film, acc|
+        film_rating = array.each_with_object({ rating_sum: 0.0, films_count: 0 }) do |film, acc|
           if is_correct_film(film)
-            acc[:raiting_sum] += film['rating_kinopoisk'].to_f
-            acc[:count] += 1
+            acc[:rating_sum] += film['rating_kinopoisk'].to_f
+            acc[:films_count] += 1
           end
         end
 
-        film_raiting[:raiting_sum] / film_raiting[:count]
+        film_rating[:rating_sum] / film_rating[:films_count]
       end
 
       def chars_count(_films, _threshold)
