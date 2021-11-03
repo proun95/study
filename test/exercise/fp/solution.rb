8df44261f249b7bd1e14ccb792e3e52ba3ab00ed
Aleksandr Sirotin <proun95@gmail.com>
2021-11-03 18:16:29 +0400

    add raiting method

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -4,8 +4,19 @@ module Exercise
       # Обратиться к параметрам фильма можно так:
       # film["name"], film["rating_kinopoisk"], film["rating_imdb"],
       # film["genres"], film["year"], film["access_level"], film["country"]
-      def rating(_array)
-        0
+      def is_correct_film(film)
+        !film['country'].nil? && film['country'].include?(',') && !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f > 0 ? true : false
+      end
+
+      def rating(array)
+        film_raiting = array.each_with_object({ raiting_sum: 0.0, count: 0 }) do |film, acc|
+          if is_correct_film(film)
+            acc[:raiting_sum] += film['rating_kinopoisk'].to_f
+            acc[:count] += 1
+          end
+        end
+
+        film_raiting[:raiting_sum] / film_raiting[:count]
       end
 
       def chars_count(_films, _threshold)
