43a37977724f1861316b4ad45f97d1efaf001e76
Aleksandr Sirotin <proun95@gmail.com>
2021-11-07 18:59:00 +0400

    fix with select method

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -12,23 +12,19 @@ module Exercise
       end
 
       def rating(array)
-        film_rating = array.each_with_object({ rating_sum: 0.0, films_count: 0 }) do |film, acc|
-          if is_correct_film(film)
-            acc[:rating_sum] += film['rating_kinopoisk'].to_f
-            acc[:films_count] += 1
-          end
+        film_rating = array.select { |film| is_correct_film(film) }.each_with_object({ rating_sum: 0.0, films_count: 0 }) do |film, acc|
+          acc[:rating_sum] += film['rating_kinopoisk'].to_f
+          acc[:films_count] += 1
         end
 
         film_rating[:rating_sum] / film_rating[:films_count]
       end
 
       def chars_count(films, threshold)
-        films.reduce(0) do |i_common_count, film|
-          if !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f >= threshold
-            i_common_count += film['name'].split('').reduce(0) do |i_film_count, name_letter|
-              i_film_count += 1 if name_letter == 'и'
-              i_film_count
-            end
+        films.select { |film| !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f >= threshold }.reduce(0) do |i_common_count, film|
+          i_common_count += film['name'].split('').reduce(0) do |i_film_count, name_letter|
+            i_film_count += 1 if name_letter == 'и'
+            i_film_count
           end
 
           i_common_count
