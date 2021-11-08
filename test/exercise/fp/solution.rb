a6966f2597e823565431285f9facd5e1d002f68b
Aleksandr Sirotin <proun95@gmail.com>
2021-11-08 18:58:48 +0400

    imrove chart_count method

--- test/exercise/fp/solution.rb
+++ test/exercise/fp/solution.rb
@@ -12,22 +12,21 @@ module Exercise
       end
 
       def rating(array)
-        film_rating = array.select { |film| is_correct_film(film) }.each_with_object({ rating_sum: 0.0, films_count: 0 }) do |film, acc|
-          acc[:rating_sum] += film['rating_kinopoisk'].to_f
-          acc[:films_count] += 1
-        end
+        correct_films = array.select { |film| is_correct_film(film) }
+        rating_sum = correct_films.reduce(0) { |sum, film| sum + film['rating_kinopoisk'].to_f }
 
-        film_rating[:rating_sum] / film_rating[:films_count]
+        rating_sum / correct_films.length
       end
 
       def chars_count(films, threshold)
-        films.select { |film| !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f >= threshold }.reduce(0) do |i_common_count, film|
-          i_common_count += film['name'].split('').reduce(0) do |i_film_count, name_letter|
-            i_film_count += 1 if name_letter == 'и'
-            i_film_count
-          end
+        correct_films_names = films.inject('') do |acc, film|
+          acc << film['name'] if !film['rating_kinopoisk'].nil? && film['rating_kinopoisk'].to_f >= threshold
+          acc
+        end
 
-          i_common_count
+        correct_films_names.split('').reduce(0) do |i_film_count, name_letter|
+          i_film_count += 1 if name_letter == 'и'
+          i_film_count
         end
       end
     end
